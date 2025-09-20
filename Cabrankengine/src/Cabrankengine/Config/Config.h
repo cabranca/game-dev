378d947f593c65afc44c380ad0b3f0707568bba1
cabranca <joaquincabrera98@gmail.com>
2025-09-20 00:30:52 -0300

    Add tab

--- Cabrankengine/src/Cabrankengine/Config/Config.h
+++ Cabrankengine/src/Cabrankengine/Config/Config.h
@@ -9,42 +9,42 @@
 namespace cabrankengine {
 
 	class Config {
-	public:
-		static nlohmann::json load(const std::string& path) {
-			std::ifstream file(path);
-			if (!file.is_open()) {
-				nlohmann::json defaults = {
-					{"window", {
-						{"title", "Cabrankengine"},
-						{"width", 1600},
-						{"height", 900}
-					}}
-				};
+		public:
+			static nlohmann::json load(const std::string& path) {
+				std::ifstream file(path);
+				if (!file.is_open()) {
+					nlohmann::json defaults = {
+						{"window", {
+							{"title", "Cabrankengine"},
+							{"width", 1600},
+							{"height", 900}
+						}}
+					};
 
-				std::ofstream out(path);
-				if (out.is_open()) {
-					out << defaults.dump(4);
-					out.close();
-					CE_CORE_WARN("[Config] File not found, creating new one in {0}", path);
+					std::ofstream out(path);
+					if (out.is_open()) {
+						out << defaults.dump(4);
+						out.close();
+						CE_CORE_WARN("[Config] File not found, creating new one in {0}", path);
+					}
+					else {
+						CE_CORE_ERROR("[Config] File not found, failed to create a new on in {0}", path);
+					}
+
+					return defaults;
 				}
-				else {
-					CE_CORE_ERROR("[Config] File not found, failed to create a new on in {0}", path);
+
+				nlohmann::json j;
+				try {
+					file >> j;
 				}
-
-				return defaults;
+				catch (std::exception& e) {
+					CE_CORE_ERROR("[Config] Failed reading {0} - Error message: {1}", path, e.what());
+					return {
+						{"window", {{"title", "Cabrankengine"}, {"width", 1600}, {"height", 900}}}
+					};
+				}
+				return j;
 			}
-
-			nlohmann::json j;
-			try {
-				file >> j;
-			}
-			catch (std::exception& e) {
-				CE_CORE_ERROR("[Config] Failed reading {0} - Error message: {1}", path, e.what());
-				return {
-					{"window", {{"title", "Cabrankengine"}, {"width", 1600}, {"height", 900}}}
-				};
-			}
-			return j;
-		}
 	};
 }
