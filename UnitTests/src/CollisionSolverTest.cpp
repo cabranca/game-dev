c2ab93439b76e9f4aa71e571580834cb51ab76c0
cabranca <joaquincabrera98@gmail.com>
2025-09-25 00:26:43 -0300

    Add unit tests

--- UnitTests/src/CollisionSolverTest.cpp
+++ UnitTests/src/CollisionSolverTest.cpp
@@ -57,6 +57,19 @@ TEST_CASE("AABB vs AABB - 2D No Collision") {
     REQUIRE_FALSE(result.collided);
 }
 
+TEST_CASE("AABB vs AABB - 3D Contact (no overlapping)") {
+    glm::vec3 pos1(0, 0, 0);
+    auto b1 = MakeBox(glm::vec3(1, 1, 1));
+
+    glm::vec3 pos2(1, 1, 1);
+    auto b2 = MakeBox(glm::vec3(1, 1, 1));
+
+    auto result = AABB(pos1, b1, pos2, b2);
+
+    REQUIRE(result.collided);
+    REQUIRE(result.penetration == 0.0f);
+}
+
 TEST_CASE("AABB vs AABB - 3D Overlap") {
     glm::vec3 pos1(0, 0, 0);
     auto b1 = MakeBox(glm::vec3(1, 1, 1));
@@ -85,6 +98,16 @@ TEST_CASE("AABB vs AABB - 3D No Collision") {
 // ====================================================
 // Sphere vs Sphere
 // ====================================================
+TEST_CASE("Sphere vs Sphere - 2D Contact (no overlapping)") {
+    glm::vec2 pos1(0, 0);
+    glm::vec2 pos2(2, 0);
+
+    auto result = Sphere(pos1, 1.0f, pos2, 1.0f);
+
+    REQUIRE(result.collided);
+    REQUIRE(result.penetration == Approx(0.0f).margin(1e-5));
+}
+
 TEST_CASE("Sphere vs Sphere - 2D Overlap") {
     glm::vec2 pos1(0, 0);
     glm::vec2 pos2(1, 0);
@@ -102,6 +125,16 @@ TEST_CASE("Sphere vs Sphere - 2D No Collision") {
     auto result = Sphere(pos1, 1.0f, pos2, 1.0f);
 
     REQUIRE_FALSE(result.collided);
+}
+
+TEST_CASE("Sphere vs Sphere - 3D Contact (no overlapping)") {
+    glm::vec3 pos1(0, 0, 0);
+    glm::vec3 pos2(2, 0, 0);
+
+    auto result = Sphere(pos1, 1.0f, pos2, 1.0f);
+
+    REQUIRE(result.collided);
+    REQUIRE(result.penetration == Approx(0.0f).margin(1e-5));
 }
 
 TEST_CASE("Sphere vs Sphere - 3D Overlap") {
@@ -126,6 +159,19 @@ TEST_CASE("Sphere vs Sphere - 3D No Collision") {
 // ====================================================
 // Sphere vs AABB
 // ====================================================
+TEST_CASE("Sphere vs AABB - 2D Contact (no overlapping)") {
+    glm::vec2 circlePos(1, 0);
+    float radius = 0.5f;
+
+    glm::vec2 boxPos(0, 0);
+    auto box = MakeBox(glm::vec2(1, 1));
+
+    auto result = SphereAABB(circlePos, radius, boxPos, box);
+
+    REQUIRE(result.collided);
+    REQUIRE(result.penetration == 0.0f);
+}
+
 TEST_CASE("Sphere vs AABB - 2D Overlap") {
     glm::vec2 circlePos(0.5f, 0.5f);
     float radius = 1.0f;
@@ -149,6 +195,19 @@ TEST_CASE("Sphere vs AABB - 2D No Collision") {
     auto result = SphereAABB(circlePos, radius, boxPos, box);
 
     REQUIRE_FALSE(result.collided);
+}
+
+TEST_CASE("Sphere vs AABB - 3D Contact (no overlapping)") {
+    glm::vec3 spherePos(1, 0, 0);
+    float radius = 0.5f;
+
+    glm::vec3 boxPos(0, 0, 0);
+    auto box = MakeBox(glm::vec3(1, 1, 1));
+
+    auto result = SphereAABB(spherePos, radius, boxPos, box);
+
+    REQUIRE(result.collided);
+    REQUIRE(result.penetration == 0.0f);
 }
 
 TEST_CASE("Sphere vs AABB - 3D Overlap") {
