939cf1f88b3e24678fd25a4e60678ca973cccce5
cabranca <joaquincabrera98@gmail.com>
2025-09-30 01:05:07 -0300

    Polish ECS

--- Cabrankengine/src/Cabrankengine/ECS/ComponentManager.hpp
+++ Cabrankengine/src/Cabrankengine/ECS/ComponentManager.hpp
@@ -7,30 +7,40 @@
 
 namespace cabrankengine {
 
-	// I do not see why there is an interface for an only child
+	/// <summary>
+	/// This interface is needed because the ComponentManager has a collection of templated component arrays.
+	/// In order to keep it generic, the collection must be of an abstract type.
+	/// </summary>
 	class IComponentArray {
 		public:
 			virtual ~IComponentArray() = default;
+
+			// This must be called after a call to EntityManager::destroyEntity()
 			virtual void entityDestroyed(Entity e) = 0;
 	};
 
+	/// <summary>
+	/// A Component Array represents a collection of the entities that hold the component of type T
+	/// </summary>
 	template<typename T>
 	class ComponentArray : public IComponentArray {
 		public:
+			// Associates the given component and entity
 			void insert(Entity e, T component) {
 				CE_CORE_ASSERT(!m_EntityToIndex.contains(e), "The component has already been added!");
-				size_t index = m_Size
+				size_t index = m_Size;
 				m_EntityToIndex[e] = index;
 				m_IndexToEntity[index] = e;
 				m_Components[index] = component;
 				m_Size++;
 			}
 
+			// Removes the component related to the given entity and any record of it
 			void remove(Entity e) {
 				CE_CORE_ASSERT(m_EntityToIndex.contains(e), "The component to remove does not exist!");
 				size_t index = m_EntityToIndex[e];
 				size_t lastIndex = m_Size - 1;
-				m_Components[index] = components[lastIndex];
+				m_Components[index] = m_Components[lastIndex];
 				Entity lastEntity = m_IndexToEntity[lastIndex];
 				m_EntityToIndex[lastEntity] = index;
 				m_IndexToEntity[index] = lastEntity;
@@ -39,25 +49,34 @@ namespace cabrankengine {
 				m_Size--;
 			}
 
-			T& get(Entity e) {
+			// Returns the component for the given entity
+			std::optional<T*> get(Entity e) {
 				CE_CORE_ASSERT(m_EntityToIndex.contains(e), "The component was not found!");
-				return m_Components[m_EntityToIndex[e]];
+				if (m_EntityToIndex.contains(e))
+					return &m_Components[m_EntityToIndex[e]];
+				else
+					return std::nullopt;
 			}
 
+			// This must be called after a call to EntityManager::destroyEntity()
 			void entityDestroyed(Entity e) override {
 				if (m_EntityToIndex.contains(e))
 					remove(e);
 			}
 
 		private:
-			std::array<T, MAX_ENTITIES> m_Components{};
-			std::unordered_map<Entity, size_t> m_EntityToIndex{};
-			std::unordered_map<size_t, Entity> m_IndexToEntity{};
-			size_t m_Size = 0;
+			std::array<T, MAX_ENTITIES> m_Components{}; // The actual component array
+			std::unordered_map<Entity, size_t> m_EntityToIndex; // Map from entity ID to the index of the component in the array
+			std::unordered_map<size_t, Entity> m_IndexToEntity; // Map from the index of the component in the array to the entity ID
+			size_t m_Size = 0; // The current amount of valid components in the array
 	};
 
+	/// <summary>
+	/// Manages registration, storage, and access of components for entities in an entity-component system.
+	/// </summary>
 	class ComponentManager {
 		public:
+			// Registers a component type T and assigns it a unique ID
 			template<typename T>
 			void registerComponent() {
 				const char* typeName = typeid(T).name();
@@ -66,6 +85,7 @@ namespace cabrankengine {
 				m_ComponentArrays[typeName] = std::make_shared<ComponentArray<T>>();
 			}
 
+			// Returns the unique ID assigned to the component type T
 			template<typename T>
 			uint8_t getComponentType() {
 				const char* typeName = typeid(T).name();
@@ -73,32 +93,36 @@ namespace cabrankengine {
 				return m_ComponentTypes[typeName];
 			}
 
+			// Adds a component of type T to the given entity
 			template<typename T>
 			void addComponent(Entity e, T component) {
 				getComponentArray<T>()->insert(e, component);
 			}
-
+			
+			// Removes the component of type T from the given entity
 			template<typename T>
 			void removeComponent(Entity e) {
 				getComponentArray<T>()->remove(e);
 			}
 
+			// Returns a reference to the component of type T associated with the given entity
 			template<typename T>
-			T& getComponent(Entity e) {
+			std::optional<T*> getComponent(Entity e) {
 				return getComponentArray<T>()->get(e);
 			}
 
+			// This must be called after a call to EntityManager::destroyEntity()
 			void entityDestroyed(Entity e) {
 				for (auto const& pair : m_ComponentArrays)
 					pair.second->entityDestroyed(e);
 			}
 
 		private:
-			std::unordered_map<const char*, uint8_t> m_ComponentTypes{};
-			std::unordered_map<const char*, std::shared_ptr<IComponentArray>> m_ComponentArrays{};
-			uint8_t m_NextComponentType = 0;
+			std::unordered_map<const char*, uint8_t> m_ComponentTypes; // Map from component type string to its ID
+			std::unordered_map<const char*, std::shared_ptr<IComponentArray>> m_ComponentArrays; // Map from component type string to its component array
+			uint8_t m_NextComponentType = 0; // The ID to be assigned to the next registered component type
 
-			// Learn more about static_pointer_cast
+			// Returns the component array for the component type T
 			template<typename T>
 			std::shared_ptr<ComponentArray<T>> getComponentArray() {
 				const char* typeName = typeid(T).name();
---
+++ Cabrankengine/src/Cabrankengine/ECS/SystemManager.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <Cabrankengine/Core/Logger.h>
+#include <Cabrankengine/ECS/Common.h>
+
+namespace cabrankengine {
+
+	class Registry;
+
+	/// <summary>
+	/// Represents a system containing a set of entities.
+	/// </summary>
+	class ISystem {
+		public:
+			virtual ~ISystem() = default;
+
+			// Update method to be implemented by derived systems
+			virtual void update(Registry& registry, float dt) = 0;
+
+			// Returns the entities managed by this system
+			const std::unordered_set<Entity>& getEntities() const { return m_Entities; }
+			
+			// Adds the given entity to the system
+			void addEntity(Entity entity) { m_Entities.emplace(entity); }
+
+			// Removes the given entity from the system
+			void removeEntity(Entity entity) { m_Entities.erase(entity); }
+
+		protected:
+			std::unordered_set<Entity> m_Entities; // Set of entities managed by the system
+	};
+
+	/// <summary>
+	/// SystemManager is responsible for managing and updating all systems in the ECS architecture.
+	/// It holds a collection of systems and provides methods to add, remove, and update them.
+	/// </summary>
+	class SystemManager {
+		public:
+			// Registers a system of type T and returns a shared pointer to it
+			template<typename T>
+			std::shared_ptr<T> RegisterSystem() {
+				const char* typeName = typeid(T).name();
+
+				CE_CORE_ASSERT(!m_Systems.contains(typeName), "System already registered!");
+
+				auto system = std::make_shared<T>();
+				m_Systems.emplace(typeName, system);
+				return system;
+			}
+
+			// Sets the signature for the system of type T
+			template<typename T>
+			void SetSignature(Signature signature) {
+				const char* typeName = typeid(T).name();
+
+				CE_CORE_ASSERT(m_Systems.contains(typeName), "System not registered!");
+
+				m_Signatures.emplace(typeName, signature);
+			}
+
+			// This must be called after a call to EntityManager::destroyEntity()
+			void EntityDestroyed(Entity entity) {
+				for (auto const& [_, system] : m_Systems) {
+					system->removeEntity(entity);
+				}
+			}
+
+			// This must be called after a call to EntityManager::setSignature()
+			void EntitySignatureChanged(Entity entity, Signature entitySignature) {
+				for (auto const& [type, system] : m_Systems) {
+					auto const& systemSignature = m_Signatures[type];
+
+					if ((entitySignature & systemSignature) == systemSignature)
+						system->addEntity(entity);
+					else
+						system->removeEntity(entity);
+				}
+			}
+
+		private:
+			std::unordered_map<const char*, Signature> m_Signatures;
+			std::unordered_map<const char*, std::shared_ptr<ISystem>> m_Systems;
+	};
+}
