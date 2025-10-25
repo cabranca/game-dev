970fa90929239479da677b63de95aab379e20f18
cabranca <joaquincabrera98@gmail.com>
2025-10-25 17:50:25 -0300

    Add Vector struct

---
+++ Cabrankengine/src/Cabrankengine/Math/Vector.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <cmath>
+
+namespace cabrankengine::math {
+
+    constexpr float EPSILON = 1e-5f;
+
+	struct Vector {
+		float x, y, z;
+
+		constexpr Vector() : x(), y(), z() {}
+
+		constexpr Vector(float uniform) : x(uniform), y(uniform), z(uniform) {}
+
+		constexpr Vector(float x, float y, float z) : x(x), y(y), z(z) {}
+
+		float length() const noexcept {
+			return sqrt(x * x + y * y + z * z);
+		}
+
+		void normalize() noexcept {
+			float lengthSquared = x * x + y * y + z * z; 
+			if (lengthSquared == 0)
+				return;
+			float length = sqrt(lengthSquared);
+			x /= length;
+			y /= length;
+			z /= length;
+		}
+
+		bool isNormalized() const noexcept {
+			float result = abs(x * x + y * y + z * z - 1.f);
+			return result < EPSILON;
+		}
+
+		constexpr Vector operator+(const Vector& other) const noexcept {
+			return { x + other.x, y + other.y, z + other.z };
+		}
+
+		constexpr Vector operator-(const Vector& other) const noexcept {
+			return { x - other.x, y - other.y, z - other.z };
+		}
+
+		constexpr Vector& operator+=(const Vector& other) noexcept {
+			x += other.x;
+			y += other.y;
+			z += other.z;
+			return *this;
+		}
+
+		constexpr Vector& operator-=(const Vector& other) noexcept {
+			x -= other.x;
+			y -= other.y;
+			z -= other.z;
+			return *this;
+		}
+
+		constexpr float dot(const Vector& other) const noexcept {
+			return x * other.x + y * other.y + z * other.z;
+		}
+
+		constexpr Vector cross(const Vector& other) const noexcept {
+			Vector res{};
+			res.x = y * other.z - z * other.y;
+			res.y = z * other.x - x * other.z;
+			res.z = x * other.y - y * other.x;
+			return res;
+		}
+	};
+
+} // namespace cabrankengine::math
