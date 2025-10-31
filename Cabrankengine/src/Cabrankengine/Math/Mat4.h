58286bb2c5fd84b91d9a4b76d575876031e3dd1e
cabranca <joaquincabrera98@gmail.com>
2025-10-31 01:05:50 -0300

    Update Mat4

--- Cabrankengine/src/Cabrankengine/Math/Mat4.h
+++ Cabrankengine/src/Cabrankengine/Math/Mat4.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include "Constants.h"
 #include "Vector3.h"
 
@@ -9,27 +11,37 @@ namespace cabrankengine::math {
     // The last column is elided as it is trivial in computation.
 
 	struct Mat4 {
-		float ix, iy, iz; // 0 //
-		float jx, jy, jz; // 0 //
-		float kx, ky, kz; // 0 //
-		float tx, ty, tz; // 1 // The t vector represents the translation from the origin
+        std::array<Vector3, 4> elements;
 
 		static const Mat4 Zero;
 		static const Mat4 Identity;
+		static const float Rows;
+		static const float Columns;
 
-		constexpr Mat4 translate(const Vector3& vec) noexcept;
-		constexpr Mat4 scale(float uniform) noexcept;
-		constexpr Mat4 scale(const Vector3& vec) noexcept;
+		static constexpr Mat4 translate(const Vector3& vec) noexcept;
+		static constexpr Mat4 scale(float uniform) noexcept;
+		static constexpr Mat4 scale(const Vector3& vec) noexcept;
+		constexpr Mat4 transpose() const noexcept;
 
-		constexpr Mat4() noexcept : ix(), iy(), iz(), jx(), jy(), jz(), kx(), ky(), kz(), tx(), ty(), tz() {}
+		constexpr Mat4() noexcept
+            : elements{
+                Vector3(),
+                Vector3(),
+                Vector3()
+            } {}
+
 		constexpr Mat4(float ix, float iy, float iz, float jx, float jy, float jz, float kx, float ky, float kz, float tx, float ty,
 		               float tz) noexcept
-		    : ix(), iy(), iz(), jx(), jy(), jz(), kx(), ky(), kz(), tx(), ty(), tz() {}
+		    : elements{
+                Vector3(ix, iy, iz),
+                Vector3(jx, jy, jz),
+                Vector3(kx, ky, kz),
+                Vector3(tx, ty, tz)
+            } {}
 
 		constexpr bool operator==(const Mat4& other) const noexcept;
 		constexpr bool operator!=(const Mat4& other) const noexcept;
-		constexpr Mat4 operator*(float scale) const noexcept;
-		constexpr Mat4& operator*=(float scale) noexcept;
+		constexpr Mat4 operator*(const Mat4& other) const noexcept;
 	};
 
 	inline constexpr Mat4 Mat4::Zero{};
@@ -39,8 +51,10 @@ namespace cabrankengine::math {
         0.f, 0.f, 1.f,
         0.f, 0.f, 0.f
     };
+	inline constexpr float Mat4::Rows{ 4 };
+	inline constexpr float Mat4::Columns{ 3 };
 
-    inline constexpr Mat4 Mat4::translate(const Vector3& vec) {
+    inline constexpr Mat4 Mat4::translate(const Vector3& vec) noexcept {
 		return {
              0.f,   0.f,   0.f,
              0.f,   0.f,   0.f,
@@ -49,7 +63,7 @@ namespace cabrankengine::math {
         };
     }
 
-    inline constexpr Mat4 Mat4::scale(float uniform) {
+    inline constexpr Mat4 Mat4::scale(float uniform) noexcept {
 		return {
             uniform,   0.f,     0.f,
               0.f,   uniform,   0.f,
@@ -58,7 +72,7 @@ namespace cabrankengine::math {
         };
 	}
 
-    inline constexpr Mat4 Mat4::scale(const Vector3& vec) {
+    inline constexpr Mat4 Mat4::scale(const Vector3& vec) noexcept {
 		return {
             vec.x,  0.f,   0.f,
              0.f,  vec.y,  0.f,
@@ -67,14 +81,33 @@ namespace cabrankengine::math {
         };
 	}
 
+    inline constexpr Mat4 Mat4::transpose() const noexcept {
+		auto& e = elements;
+		return {
+            e[0].x, e[1].x, e[2].x,
+            e[0].y, e[1].y, e[2].y,
+            e[0].z, e[1].z, e[2].z,
+            0.f, 0.f, 0.f
+        };
+    }
+
 	constexpr bool Mat4::operator==(const Mat4& other) const noexcept {
-		return ix == other.ix && iy == other.iy && iz == other.iz && jx == other.jx && jy == other.jy && jz == other.jz && kx == other.kx &&
-		       ky == other.ky && kz == other.kz && tx == other.tx && ty == other.ty && tz == other.tz;
+		return elements == other.elements;
 	}
 
 	constexpr bool Mat4::operator!=(const Mat4& other) const noexcept {
-		return ix == other.ix && iy == other.iy && iz == other.iz && jx == other.jx && jy == other.jy && jz == other.jz && kx == other.kx &&
-		       ky == other.ky && kz == other.kz && tx == other.tx && ty == other.ty && tz == other.tz;
+		return elements != other.elements;
 	}
+
+    constexpr Mat4 Mat4::operator*(const Mat4& other) const noexcept {
+		Mat4 transposed = other.transpose();
+		Mat4 res{};
+		for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Columns; j++) {
+				res.elements[i][j] = dot(elements[i], transposed.elements[j]);
+            }
+        }
+		return res;
+    }
 
 } // namespace cabrankengine::math
