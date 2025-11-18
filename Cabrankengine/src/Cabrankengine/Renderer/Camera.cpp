924fe2f564b464cb73f87f16beaed19be1622664
cabranca <joaquincabrera98@gmail.com>
2025-11-18 02:23:41 -0300

    Add Base Camera class.

---
+++ Cabrankengine/src/Cabrankengine/Renderer/Camera.cpp
@@ -0,0 +1,25 @@
+#include "Camera.h"
+
+namespace cabrankengine {
+
+	Camera::Camera(const math::Mat4& projectionMatrix, const math::Mat4& viewMatrix) noexcept
+	    : m_Transform(math::identityMat()), m_ProjectionMatrix(projectionMatrix), m_ViewMatrix(viewMatrix) {}
+
+	const math::Mat4& Camera::getTransform() const noexcept {
+		return m_Transform;
+	}
+
+	const void Camera::setTransform(const math::Mat4& transform) noexcept {
+		m_Transform = transform;
+		m_ViewProjectionMatrix = m_ProjectionMatrix * inverse(m_Transform);
+	}
+
+	const void Camera::setTransform(const math::Vector3& position, const math::Vector3& rotation, const math::Vector3& scale) noexcept {
+		m_Transform = math::translate(position) * math::rotate(rotation) * math::scale(scale);
+		m_ViewProjectionMatrix = m_ProjectionMatrix * inverse(m_Transform);
+	}
+
+	const math::Mat4& Camera::getViewProjectionMatrix() const noexcept {
+		return m_ViewProjectionMatrix;
+	}
+} // namespace cabrankengine
---
+++ Cabrankengine/src/Cabrankengine/Renderer/Camera.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Cabrankengine/Math/MatrixFactory.h>
+
+namespace cabrankengine {
+
+	class Camera {
+	  public:
+		Camera(const math::Mat4& projectionMatrix, const math::Mat4& ViewMatrix) noexcept;
+
+		const math::Mat4& getTransform() const noexcept;
+		const void setTransform(const math::Mat4& transform) noexcept;
+		const void setTransform(const math::Vector3& position, const math::Vector3& rotation, const math::Vector3& scale) noexcept;
+		const math::Mat4& getViewProjectionMatrix() const noexcept;
+
+	  protected:
+		math::Mat4 m_Transform;
+		math::Mat4 m_ProjectionMatrix;
+		math::Mat4 m_ViewMatrix;
+		math::Mat4 m_ViewProjectionMatrix;
+	};
+} // namespace cabrankengine
