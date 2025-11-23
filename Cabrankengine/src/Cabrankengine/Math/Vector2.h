92fb218b0629c17b4e77ffc8bf72c60d7db23405
cabranca <joaquincabrera98@gmail.com>
2025-11-23 15:52:32 -0300

    Add Vector2 implementation.

---
+++ Cabrankengine/src/Cabrankengine/Math/Vector2.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include <cmath>
+#include <algorithm>
+
+#include <Cabrankengine/Core/Logger.h>
+
+#include "Constants.h"
+
+namespace cabrankengine::math {
+
+    // This Vector implementation uses row convention: The vector is a 1x3 matrix
+	struct Vector2 {
+		float x, y;
+
+		static const Vector2 Right;
+		static const Vector2 Left;
+		static const Vector2 Up;
+		static const Vector2 Down;
+		static const Vector2 Zero;
+		static const Vector2 One;
+
+		constexpr Vector2() noexcept : x(), y() {}
+		explicit constexpr Vector2(float uniform) noexcept : x(uniform), y(uniform) {}
+		constexpr Vector2(float x, float y) noexcept : x(x), y(y) {}
+
+		constexpr float lengthSquared() const noexcept;
+		float length() const noexcept;
+		void normalize() noexcept;
+		bool isNormalized() const noexcept;
+
+		constexpr bool operator==(const Vector2& other) const noexcept;
+		constexpr bool operator!=(const Vector2& other) const noexcept;
+		constexpr Vector2 operator+(const Vector2& other) const noexcept;
+		constexpr Vector2& operator+=(const Vector2& other) noexcept;
+		constexpr Vector2 operator-(const Vector2& other) const noexcept;
+		constexpr Vector2& operator-=(const Vector2& other) noexcept;
+		constexpr Vector2 operator-() noexcept;
+		constexpr Vector2 operator*(float scale) const noexcept;
+		constexpr Vector2& operator*=(float scale) noexcept;
+		constexpr float& operator[](int index) noexcept;
+		constexpr const float& operator[](int index) const noexcept;
+	};
+
+    inline constexpr Vector2 Vector2::Right     { (1.f), (0.f) };
+	inline constexpr Vector2 Vector2::Left      { (-1.f), (0.f) };
+	inline constexpr Vector2 Vector2::Up        { (0.f), (1.f) };
+	inline constexpr Vector2 Vector2::Down      { (0.f), (-1.f) };
+	inline constexpr Vector2 Vector2::Zero      { (0.f) };
+	inline constexpr Vector2 Vector2::One       { (1.f) };
+
+    inline constexpr float Vector2::lengthSquared() const noexcept {
+		return x * x + y * y;
+	}
+
+	inline float Vector2::length() const noexcept {
+		return sqrt(lengthSquared());
+	}
+
+	inline void Vector2::normalize() noexcept {
+		float squared = lengthSquared();
+		if (squared == 0)
+			return;
+		float length = sqrt(squared);
+		x /= length;
+		y /= length;
+	}
+
+	inline bool Vector2::isNormalized() const noexcept {
+		float result = abs(lengthSquared() - 1.f);
+		return result < EPSILON;
+	}
+
+	inline constexpr bool Vector2::operator==(const Vector2& other) const noexcept {
+		return x == other.x && y == other.y;
+	}
+
+	inline constexpr bool Vector2::operator!=(const Vector2& other) const noexcept {
+		return x != other.x || y != other.y;
+	}
+
+	inline constexpr Vector2 Vector2::operator+(const Vector2& other) const noexcept {
+		return { x + other.x, y + other.y };
+	}
+
+	inline constexpr Vector2& Vector2::operator+=(const Vector2& other) noexcept {
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+
+	inline constexpr Vector2 Vector2::operator-(const Vector2& other) const noexcept {
+		return { x - other.x, y - other.y };
+	}
+
+	inline constexpr Vector2& Vector2::operator-=(const Vector2& other) noexcept {
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+
+	inline constexpr Vector2 Vector2::operator-() noexcept {
+		return { -x, -y };
+	}
+
+	inline constexpr Vector2 Vector2::operator*(float scale) const noexcept {
+		return { x * scale, y * scale };
+	}
+
+	inline constexpr Vector2& Vector2::operator*=(float scale) noexcept {
+		x *= scale;
+		y *= scale;
+		return *this;
+	}
+
+	inline constexpr float& Vector2::operator[](int index) noexcept {
+        switch (index) {
+		    case 0:
+			    return x;
+		    case 1:
+			    return y;
+		    default:
+			    CE_CORE_ERROR("Trying to acces a Vector with invalid index!");
+			    return x; // This must be handled in other way
+        }
+	}
+
+    inline constexpr const float& Vector2::operator[](int index) const noexcept {
+		switch (index) {
+		    case 0:
+			    return x;
+		    case 1:
+			    return y;
+		    default:
+			    CE_CORE_ERROR("Trying to access Vector4 with invalid index!");
+			    return x;
+		}
+	}
+
+	inline constexpr float dot(const Vector2& a, const Vector2& b) noexcept {
+		return a.x * b.x + a.y * b.y;
+	}
+
+	inline constexpr bool perpendicular(const Vector2& a, const Vector2& b) noexcept {
+		return dot(a, b) == 0;
+	}
+
+    inline float angleBetween(const Vector2& a, const Vector2& b) noexcept {
+		float denom = a.length() * b.length();
+		if (denom == 0.f)
+			return 0.f;
+		float cosTheta = dot(a, b) / denom;
+		cosTheta = std::clamp(cosTheta, -1.f, 1.f);
+		return std::acos(cosTheta);
+	}
+
+} // namespace cabrankengine::math
