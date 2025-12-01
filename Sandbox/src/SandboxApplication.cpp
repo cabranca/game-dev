15f0e86b39c00282d19c1abc20126003b60f028e
cabranca <joaquincabrera98@gmail.com>
2025-12-01 09:27:13 -0300

    Set FPS controller example.

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -15,14 +15,51 @@ using namespace cabrankengine::platform::opengl;
 
 class ExampleLayer : public Layer {
   public:
-	ExampleLayer() : Layer("Example"), m_Camera(-16.f / 9.f, 16.f / 9.f, -1.f, 1.f) {
+	ExampleLayer() : Layer("Example"), m_CameraController(PerspectiveCamera(-16.f / 9.f, 16.f / 9.f, 0.1f, 100.f)) {
 		m_SquareVA = VertexArray::create();
 
-		float squareVertices[5 * 4] = {
-			 0.5f,  0.5f,  0.0f,    1.0f, 1.0f,
-			 0.5f, -0.5f,  0.0f,    1.0f, 0.0f,
-			-0.5f, -0.5f,  0.0f,    0.0f, 0.0f,
-			-0.5f,  0.5f,  0.0f,    0.0f, 1.0f
+		float squareVertices[5 * 36] = {
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 		};
 
 		Ref<VertexBuffer> squareVB = VertexBuffer::create(squareVertices, sizeof(squareVertices));
@@ -31,7 +68,10 @@ class ExampleLayer : public Layer {
 
 		m_SquareVA->addVertexBuffer(squareVB);
 
-		uint32_t squareIndices[6] = { 0, 1, 3, 1, 2, 3 };
+		uint32_t squareIndices[36];
+		for (int i = 0; i < 36; i++) {
+			squareIndices[i] = i;
+		}
 
 		Ref<IndexBuffer> squareIB = IndexBuffer::create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
 
@@ -47,7 +87,6 @@ class ExampleLayer : public Layer {
 
 		std::dynamic_pointer_cast<OpenGLShader>(textureShader)->uploadUniformInt("texture1", 0);
 		std::dynamic_pointer_cast<OpenGLShader>(textureShader)->uploadUniformInt("texture2", 1);
-		
 	}
 
 	void onUpdate(Timestep delta) override {
@@ -55,7 +94,9 @@ class ExampleLayer : public Layer {
 		RenderCommand::setClearColor({ 0.1f, 0.1f, 0.1f, 1.f });
 		RenderCommand::clear();
 
-		Renderer::beginScene(m_Camera);
+		m_CameraController.onUpdate(delta);
+
+		Renderer::beginScene(m_CameraController.getCamera());
 		auto textureShader = m_ShaderLibrary.get("OldTexture");
 
 		m_Texture->bind(0);
@@ -63,28 +104,41 @@ class ExampleLayer : public Layer {
 
 		textureShader->bind();
 		static float rotation = 0.f;
-		rotation += delta * 90.f;
-		Renderer::submit(textureShader, m_SquareVA, rotateX(rotation));
+		rotation += delta * 30.f;
 
-		m_LogoTexture->bind();
-
-		//Renderer::submit(textureShader, m_SquareVA);
+		for (int i = 0; i < 10; i++) {
+			Renderer::submit(textureShader, m_SquareVA, rotateXYZ(Vector3(rotation, rotation, 0.f)) * translation(cubePositions[i]));
+		}
+		
 		Renderer::endScene();
 	}
 
   private:
-	OrthographicCamera m_Camera;
+	Vector3 cubePositions[10] = {
+		Vector3( 0.0f,  0.0f,  0.0f), 
+		Vector3( 2.0f,  5.0f, -15.0f), 
+		Vector3(-1.5f, -2.2f, -2.5f),  
+		Vector3(-3.8f, -2.0f, -12.3f),  
+		Vector3( 2.4f, -0.4f, -3.5f),  
+		Vector3(-1.7f,  3.0f, -7.5f),  
+		Vector3( 1.3f, -2.0f, -2.5f),  
+		Vector3( 1.5f,  2.0f, -2.5f), 
+		Vector3( 1.5f,  0.2f, -1.5f), 
+		Vector3(-1.3f,  1.0f, -1.5f)  
+	};
+
+	CameraController m_CameraController;
 	ShaderLibrary m_ShaderLibrary;
 	Ref<Texture2D> m_Texture, m_LogoTexture;
 	Ref<VertexArray> m_SquareVA;
-	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f }; // Color of the square
+	Vector3 m_SquareColor = { 0.2f, 0.3f, 0.8f }; // Color of the square
 };
 
 class Sandbox : public Application {
   public:
 	Sandbox() {
-		//pushLayer(new ExampleLayer());
-		pushLayer(new Sandbox2D());
+		pushLayer(new ExampleLayer());
+		//pushLayer(new Sandbox2D());
 	}
 	~Sandbox() {}
 };
