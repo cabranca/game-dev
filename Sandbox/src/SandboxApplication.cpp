9b55d80adc5fa4a03fae44a2570d2858756c4ef3
cabranca <joaquincabrera98@gmail.com>
2025-12-05 18:56:40 -0300

    Update sandbox.

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -75,9 +75,14 @@ class ExampleLayer : public Layer {
 		auto lightingShader = m_ShaderLibrary.load("assets/shaders/Lightning.glsl");
 
 		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->bind();
-		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("lightPos", Vector3());
-		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("objectColor", Vector3(1.0f, 0.5f, 0.31f));
-        std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("lightColor", Vector3(1.0f, 1.0f, 1.0f));
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("material.ambient", Vector3(1.0f, 0.5f, 0.31f));
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("material.diffuse", Vector3(1.0f, 0.5f, 0.31f));
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("material.specular", Vector3(0.5f, 0.5f, 0.5f));
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat1("material.shininess", 32.f);
+
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("light.position", Vector3());
+		
+
 	}
 
 	void onUpdate(Timestep delta) override {
@@ -88,23 +93,34 @@ class ExampleLayer : public Layer {
 		m_CameraController.onUpdate(delta);
 
 		Renderer::beginScene(m_CameraController.getCamera());
-		auto textureShader = m_ShaderLibrary.get("Lightning");
-		textureShader->bind();
-		std::dynamic_pointer_cast<OpenGLShader>(textureShader)->uploadUniformFloat3("viewPos", m_CameraController.getCamera().getWorldPosition());
-		
+		auto lightingShader = m_ShaderLibrary.get("Lightning");
+		lightingShader->bind();
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("viewPos", m_CameraController.getCamera().getWorldPosition());
+
 		static float rotation = 0.f;
 		rotation += delta * 30.f;
 
-		for (int i = 0; i < 10; i++) {
-			Renderer::submit(textureShader, m_CubeVA, rotateXYZ(Vector3(rotation, rotation, 0.f)) * translation(cubeVertexPositions[i]));
+		Vector3 lightColor;
+        lightColor.x = sinf(rotation * 0.02f);
+        lightColor.y = sinf(rotation * 0.07f);
+        lightColor.z = sinf(rotation * 0.013f);
+        Vector3 diffuseColor = lightColor   * 0.5f; // decrease the influence
+        Vector3 ambientColor = diffuseColor * 0.2f; // low influence
+        std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("light.ambient", ambientColor);
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("light.diffuse", diffuseColor);
+		std::dynamic_pointer_cast<OpenGLShader>(lightingShader)->uploadUniformFloat3("light.specular", Vector3(1.f));
+		
+		
+
+		for (int i = 0; i < 9; i++) {
+			Renderer::submit(lightingShader, m_CubeVA, rotateXYZ(Vector3(rotation, rotation, 0.f)) * translation(cubeVertexPositions[i]));
 		}
 		
 		Renderer::endScene();
 	}
 
   private:
-	Vector3 cubeVertexPositions[10] = {
-		Vector3( 0.0f,  0.0f,  0.0f), 
+	Vector3 cubeVertexPositions[9] = {
 		Vector3( 2.0f,  5.0f, -15.0f), 
 		Vector3(-1.5f, -2.2f, -2.5f),  
 		Vector3(-3.8f, -2.0f, -12.3f),  
