45a55539d425f459bfda7e3f60a1acf47d9fc248
cabranca <joaquincabrera98@gmail.com>
2025-12-13 11:55:43 -0300

    Add working TextRenderer.

--- Cabrankengine/src/Cabrankengine/Renderer/TextRenderer.cpp
+++ Cabrankengine/src/Cabrankengine/Renderer/TextRenderer.cpp
@@ -1,139 +1,147 @@
-// #include "TextRenderer.h"
+#include "TextRenderer.h"
 
-// #include <ft2build.h>
-// #include FT_FREETYPE_H
-// #include <glad/glad.h>
-// #include <glm/ext/matrix_clip_space.hpp>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <glad/glad.h>
 
-// #include <Cabrankengine/Core/Logger.h>
-// #include <Cabrankengine/Math/MatrixFactory.h>
+#include <Cabrankengine/Core/Logger.h>
 
-// #include "Buffer.h"
-// #include "RenderCommand.h"
-// #include "Shader.h"
-// #include "Texture.h"
-// #include "VertexArray.h"
+#include "Buffer.h"
+#include "RenderCommand.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "VertexArray.h"
 
-// #include <iostream>
+namespace cabrankengine::rendering {
 
+	using namespace math;
 
-// using namespace glm;
+	struct TextVertex {
+		Vector3 Position;
+		Vector2 TexCoord;
+	};
 
-// namespace cabrankengine {
+	struct TextRendererData {
+		static const uint32_t maxChar = 1000;
+		static const uint32_t maxVertices = maxChar * 6;
 
+		Ref<VertexArray> textVertexArray;
+		Ref<VertexBuffer> textVertexBuffer;
+		Ref<Shader> textShader;
+	};
 
-//     TextRenderer::TextRenderer(unsigned int width, unsigned int height) : m_ShaderLibrary(std::make_unique<ShaderLibrary>()) {
-//         // load and configure shader
-//         m_TextShader = m_ShaderLibrary->load("assets/shaders/Text.glsl");
-//         m_TextShader->setMat4("projection", math::ortho(0.0f, static_cast<float>(width), static_cast<float>(height), 0.0f, -1.f, 1.f));
-//         m_TextShader->setInt("text", 0);
+	static TextRendererData s_Data;
 
-//         // configure vertex array/vertex buffer for texture quads
-//         m_VertexArray = VertexArray::create();
+	void TextRenderer::init() {
 
-//         m_VertexBuffer = VertexBuffer::create(6 * 4 * 4);
+		s_Data.textVertexArray = VertexArray::create();
+		s_Data.textVertexBuffer = VertexBuffer::create(6 * sizeof(TextVertex));
+		s_Data.textVertexBuffer->setLayout({ { ShaderDataType::Float3, "pos" }, { ShaderDataType::Float2, "texCoord" } });
+		s_Data.textVertexArray->addVertexBuffer(s_Data.textVertexBuffer);
 
-//         m_VertexBuffer->setLayout({ { ShaderDataType::Float2, "pos" }, {ShaderDataType::Float2, "tex"} });
+		s_Data.textShader = Shader::create("assets/shaders/Text.glsl");
+		s_Data.textShader->bind();
+		s_Data.textShader->setInt("text", 0);
 
-//         m_VertexArray->addVertexBuffer(m_VertexBuffer);
+		loadFont("assets/fonts/ocraext.ttf", 24.f);
+	}
 
-//         m_VertexBuffer->unbind();
-//         m_VertexArray->unbind();
-//     }
+	void TextRenderer::shutdown() {}
 
-//     void TextRenderer::load(const std::string& font, unsigned int fontSize)
-//     {
-//         // first clear the previously loaded Characters
-//         m_Characters.clear();
-        
-//         // then initialize and load the FreeType library
-//         FT_Library ft;
-//         if (FT_Init_FreeType(&ft)) // all functions return a value different than 0 whenever an error occurred
-//             CE_CORE_ERROR("ERROR::FREETYPE: Could not init FreeType Library");
-        
-//         // load font as face
-//         FT_Face face;
-//         if (FT_New_Face(ft, font.c_str(), 0, &face))
-//             CE_CORE_ERROR("ERROR::FREETYPE: Failed to load font");
-        
-//         // set size to load glyphs as
-//         FT_Set_Pixel_Sizes(face, 0, fontSize);
-        
-//         // disable byte-alignment restriction
-//         glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // TODO: Abstract away
-        
-//         GLint swizzleMask[] = { GL_ONE, GL_ONE, GL_ONE, GL_RED };
+	void TextRenderer::beginScene(const math::Mat4& viewProjection) {
+		s_Data.textShader->bind();
+		s_Data.textShader->setMat4("u_ViewProjection", viewProjection);
+	}
 
-//         // then for the first 128 ASCII characters, pre-load/compile their characters and store them
-//         for (GLubyte c = 0; c < 128; c++) // lol see what I did there 
-//         {
-//             // load character glyph 
-//             if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-//             {
-//                 CE_CORE_ERROR("ERROR::FREETYTPE: Failed to load Glyph");
-//                 continue;
-//             }
-            
-//             auto charTexture = Texture2D::create(face);
+	void TextRenderer::endScene() {
+		flush();
+	}
 
-//             // now store character for later use
-//             Character character = {
-//                 charTexture,
-//                 ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-//                 ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-//                 static_cast<unsigned int>(face->glyph->advance.x)
-//             };
-//             m_Characters.emplace(c, character);
-//         }
-        
-//         // destroy FreeType once we're finished
-//         FT_Done_Face(face);
-//         FT_Done_FreeType(ft);
-//     }
+	void TextRenderer::flush() {}
 
-//     void TextRenderer::renderText(const std::string& text, float x, float y, float scale, const Vector3& color)
-//     {
-//          // activate corresponding render state	
-//          m_TextShader->bind();
-//          m_TextShader->setFloat3("textColor", color);
-//          m_VertexArray->bind();
+	void TextRenderer::drawText(const std::string& text, math::Vector3 position, float scale, math::Vector4 color) {
+		s_Data.textShader->bind();
+		s_Data.textShader->setFloat4("textColor", color);
+		s_Data.textVertexArray->bind();
 
-//          // iterate through all characters
-//          for (char c : text)
-//          {
-//              Character ch = m_Characters.at(c);
+		// iterate through all characters
+		for (char c: text) {
+			Character ch = s_Characters.at(c);
 
-//              float xpos = x + ch.Bearing.x * scale;
-//              float ypos = y + (m_Characters['H'].Bearing.y - ch.Bearing.y) * scale;
+			float xpos = position.x + ch.Bearing.x * scale;
+			float ypos = position.y + (s_Characters['H'].Bearing.y - ch.Bearing.y) * scale;
 
-//              float w = ch.Size.x * scale;
-//              float h = ch.Size.y * scale;
-             
-//              // update VBO for each character
-//              float vertices[6][4] = {
-//                  { xpos,     ypos + h,   0.0f, 1.0f },
-//                  { xpos + w, ypos,       1.0f, 0.0f },
-//                  { xpos,     ypos,       0.0f, 0.0f },
+			float w = ch.Size.x * scale;
+			float h = ch.Size.y * scale;
 
-//                  { xpos,     ypos + h,   0.0f, 1.0f },
-//                  { xpos + w, ypos + h,   1.0f, 1.0f },
-//                  { xpos + w, ypos,       1.0f, 0.0f }
-//              };
-            
-//              // render glyph texture over quad
-//              ch.Texture->bind();
-            
-//              // update content of VBO memory
-//              m_VertexBuffer->bind();
-//              m_VertexBuffer->setData(vertices, sizeof(vertices));
-//              m_VertexBuffer->unbind();
-            
-//              // render quad
-//              RenderCommand::draw(m_VertexArray);
-            
-//              // now advance cursors for next glyph
-//              x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (1/64th times 2^6 = 64)
-//          }
-//          m_VertexArray->unbind();
-//     }
-// }
+			// update VBO for each character
+			float vertices[6][5] = {
+                { xpos,     ypos + h, position.z, 0.0f, 0.0f }, 
+                { xpos + w, ypos,     position.z, 1.0f, 1.0f },     
+                { xpos,     ypos,     position.z, 0.0f, 1.0f },
+                { xpos,     ypos + h, position.z, 0.0f, 0.0f }, 
+                { xpos + w, ypos + h, position.z, 1.0f, 0.0f }, 
+                { xpos + w, ypos,     position.z, 1.0f, 1.0f } 
+            };
+
+			// render glyph texture over quad
+			ch.Texture->bind();
+
+			// update content of VBO memory
+			s_Data.textVertexBuffer->bind();
+			s_Data.textVertexBuffer->setData(vertices, sizeof(vertices));
+			s_Data.textVertexBuffer->unbind();
+
+			// render quad
+			RenderCommand::draw(s_Data.textVertexArray);
+
+			// now advance cursors for next glyph
+			position.x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (1/64th times 2^6 = 64)
+		}
+	}
+
+	void TextRenderer::loadFont(const std::string& font, unsigned int fontSize) {
+		// first clear the previously loaded Characters
+		s_Characters.clear();
+
+		// then initialize and load the FreeType library
+		FT_Library ft;
+		if (FT_Init_FreeType(&ft)) // all functions return a value different than 0 whenever an error occurred
+			CE_CORE_ERROR("ERROR::FREETYPE: Could not init FreeType Library");
+
+		// load font as face
+		FT_Face face;
+		if (FT_New_Face(ft, font.c_str(), 0, &face))
+			CE_CORE_ERROR("ERROR::FREETYPE: Failed to load font");
+
+		// set size to load glyphs as
+		FT_Set_Pixel_Sizes(face, 0, fontSize);
+
+		// disable byte-alignment restriction
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // TODO: Abstract away
+
+		GLint swizzleMask[] = { GL_ONE, GL_ONE, GL_ONE, GL_RED };
+
+		// then for the first 128 ASCII characters, pre-load/compile their characters and store them
+		for (GLubyte c = 0; c < 128; c++) // lol see what I did there
+		{
+			// load character glyph
+			if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+				CE_CORE_ERROR("ERROR::FREETYTPE: Failed to load Glyph");
+				continue;
+			}
+
+			auto charTexture = Texture2D::create(face);
+
+			// now store character for later use
+			Character character = { charTexture, Vector2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+				                    Vector2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+				                    static_cast<unsigned int>(face->glyph->advance.x) };
+			s_Characters.emplace(c, character);
+		}
+
+		// destroy FreeType once we're finished
+		FT_Done_Face(face);
+		FT_Done_FreeType(ft);
+	}
+} // namespace cabrankengine::rendering
--- Cabrankengine/src/Cabrankengine/Renderer/TextRenderer.h
+++ Cabrankengine/src/Cabrankengine/Renderer/TextRenderer.h
@@ -1,56 +1,35 @@
-// #pragma once
+#pragma once
 
-// #include <string>
-// #include <unordered_map>
+#include <Cabrankengine/Core/Core.h>
+#include <Cabrankengine/Math/Mat4.h>
+namespace cabrankengine::rendering {
 
-// #include <glm/glm.hpp>
+    class Texture2D;
 
-// #include <Cabrankengine/Core/Core.h>
-// #include <Cabrankengine/Math/MatrixFactory.h>
+    /// Holds all state information relevant to a character as loaded using FreeType.
+	struct Character {
+		Ref<Texture2D> Texture; // ID handle of the glyph texture
+		math::Vector2 Size;     // Size of glyph
+		math::Vector2 Bearing;  // Offset from baseline to left/top of glyph
+		unsigned int Advance;   // Horizontal offset to advance to next glyph
+	};
 
-// namespace cabrankengine {
+	// A renderer class for rendering text displayed by a font loaded using the
+	// FreeType library. A single font is loaded, processed into a list of Character
+	// items for later rendering.
+	class TextRenderer {
+	  public:
+		static void init();
+        static void shutdown();
+        static void beginScene(const math::Mat4& viewProjection);
+        static void endScene();
+        static void flush();
+        static void drawText(const std::string& text, math::Vector3 position, float scale, math::Vector4 color);
 
-//     // Forward declarations
-//     class Shader;
-//     class ShaderLibrary;
-//     class Texture2D;
-//     class VertexArray;
-//     class VertexBuffer;
+		static void loadFont(const std::string& font, unsigned int fontSize);
 
-//     /// Holds all state information relevant to a character as loaded using FreeType.
-//     struct Character {
-//         Ref<Texture2D> Texture; // ID handle of the glyph texture
-//         glm::ivec2   Size;      // Size of glyph
-//         glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
-//         unsigned int Advance;   // Horizontal offset to advance to next glyph
-//     };
-
-
-//     // A renderer class for rendering text displayed by a font loaded using the 
-//     // FreeType library. A single font is loaded, processed into a list of Character
-//     // items for later rendering.
-//     class TextRenderer
-//     {
-//     public:
-//         TextRenderer(unsigned int width, unsigned int height);
-
-//         // Pre-compiles a list of characters from the given font.
-//         void load(const std::string& font, unsigned int fontSize);
-
-//         // Renders a string of text using the precompiled list of characters.
-//         void renderText(const std::string& text, float x, float y, float scale, const math::Vector3& color = math::Vector3(1.f));
-    
-//     private:
-//         // Render state.
-//         Ref<VertexBuffer> m_VertexBuffer; // Replaces older unsigned int m_Vao
-//         Ref<VertexArray> m_VertexArray; // Replaces older unsigned int m_Vbo
-
-//         // Holds a list of pre-compiled Characters.
-//         std::unordered_map<char, Character> m_Characters;
-
-//         Scope<ShaderLibrary> m_ShaderLibrary;
-//         // Shader used for text rendering.
-//         Ref<Shader> m_TextShader;
-//     };
-// }
-
+	  private:
+		// Holds a list of pre-compiled Characters.
+		inline static std::unordered_map<char, Character> s_Characters = {};
+	};
+} // namespace cabrankengine::rendering
