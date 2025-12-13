401f63a50b3888314f49ded6dbad4d4bda29b890
cabranca <joaquincabrera98@gmail.com>
2025-12-13 11:56:46 -0300

    Add Texture initialization from Text font.

--- Cabrankengine/src/Cabrankengine/Renderer/Texture.cpp
+++ Cabrankengine/src/Cabrankengine/Renderer/Texture.cpp
@@ -27,4 +27,14 @@ namespace cabrankengine::rendering {
 		CE_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
 	}
-}
+
+	Ref<Texture2D> Texture2D::create(const FT_Face& face) {
+		switch (Renderer::getAPI()) {
+		case RendererAPI::API::None:    CE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return  createRef<platform::opengl::OpenGLTexture2D>(face);
+		}
+
+		CE_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+} // namespace cabrankengine::rendering
