c6b8f2a8a85e081b570c1c649af06c83df29a74c
cabranca <joaquincabrera98@gmail.com>
2025-12-26 00:05:28 -0300

    Add Uniform buffer interface and implementation.

---
+++ Cabrankengine/src/Cabrankengine/Renderer/UniformBuffer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace cabrankengine::rendering {
+
+	class UniformBuffer {
+	  public:
+		virtual ~UniformBuffer() = default;
+
+		virtual void setData(const void* data, uint32_t size, uint32_t offset = 0) = 0;
+
+		static Ref<UniformBuffer> create(uint32_t size, uint32_t binding);
+	};
+} // namespace cabrankengine::rendering
